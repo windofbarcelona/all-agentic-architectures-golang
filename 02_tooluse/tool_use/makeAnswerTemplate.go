67c8b02d16eb48152af05df776808c160a78102a
windOfBarcelona <857297059@qq.com>
2025-12-14 13:37:20 +0800

    change prompt

--- 02_tooluse/tool_use/makeAnswerTemplate.go
+++ 02_tooluse/tool_use/makeAnswerTemplate.go
@@ -10,9 +10,12 @@ func DraftCodeTemplate() prompt.DefaultChatTemplate {
 	角色：你是一个精于解决用户问题的问题解答师。
 	任务：根据用户的需求，回答问题，必要时使用提供给你的工具进行问题回答。
 	要求：如果你已知了工具的查询结果，则无需继续调用工具，而应该结果整合进最终的回答中，确保回答的准确性和完整性。
+	注意事项：
+	1. map_directions可以用来查询出行路线和耗时
 	`
 	chatTpl := prompt.FromMessages(schema.FString,
 		schema.SystemMessage(systemTpl),
+		//schema.MessagesPlaceholder("message_histories", true),
 		schema.UserMessage("{user_query}"),
 	)
 	return *chatTpl
